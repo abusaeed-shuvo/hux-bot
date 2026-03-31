b3d4c44b4cc5ed43b322694fffaaa94dbeaa318f
Saber_0324 <bryanventura0324@gmail.com>
2026-03-30 21:30:59 -0400

    Made the command group "info" inside information cog.
    added role information commands.
    
    added the template for future role count integration

--- cogs/info.py
+++ cogs/info.py
@@ -9,8 +9,13 @@ class Info(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f"Pong {round(self.bot.latency * 1000, 2)} ms!")
     
-    @commands.command()
-    async def userinfo(self, ctx: commands.Context, user: discord.Member):
+    @commands.group()
+    async def info(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f"Correct usage: \n`!info server` \n`!info user @user` \n`!info role (count/list/@role)`")
+
+    @info.command()
+    async def user(self, ctx: commands.Context, user: discord.Member):
         embed = discord.Embed(title="User Information", color=discord.Color.blue())
         embed.set_thumbnail(url=user.avatar.url if user.avatar else user.default_avatar.url)
         embed.add_field(name="User Name", value=f"{user.name}", inline=True)
@@ -19,8 +24,8 @@ class Info(commands.Cog):
         embed.add_field(name="Account Created", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
         await ctx.send(embed=embed)
         
-    @commands.command()
-    async def serverinfo(self, ctx):
+    @info.command()
+    async def server(self, ctx):
         server = ctx.guild
         embed = discord.Embed(title="Server Information", color=discord.Color.blue())
         embed.set_thumbnail(url=server.icon.url if server.icon else None)
@@ -30,6 +35,30 @@ class Info(commands.Cog):
         embed.add_field(name="Member Count", value=server.member_count, inline=True)
         embed.add_field(name="Creation Date", value=server.created_at.strftime("%Y/%m/%d"), inline=False)
         await ctx.send(embed=embed)
-        
+
+    @info.group(invoke_without_command=True)
+    async def role(self, ctx, target: discord.Role = None):
+        if ctx.invoked_subcommand is None:
+            if target:
+                embed = discord.Embed(title="Role Info", color=target.color)
+                embed.add_field(name="Role Name", value=target.mention, inline=True)
+                embed.add_field(name="Role ID", value=target.id, inline=False)
+                embed.add_field(name="Member Count", value=len(target.members), inline=False)
+                embed.add_field(name="Role Color", value=str(target.color), inline=False)
+                await ctx.send(embed=embed)
+            else:    
+                await ctx.send(f"Correct usage: \n`!info role @role` \n`!info role list` \n`!info role count (group)`")
+
+    @role.command(name="list")
+    async def role_list(self, ctx):
+        embed = discord.Embed(title="Role List", color=discord.Color.blue())
+        embed.set_thumbnail(url=ctx.guild.icon.url if ctx.guild.icon else None)
+        embed.add_field(name="Roles", value="\n".join([r.mention for r in ctx.guild.roles][1:][::-1]), inline=False)
+        await ctx.send(embed=embed)
+
+    @role.command()
+    async def count(self, ctx, *, group: str = None):
+        pass
+    
 async def setup(bot):
     await bot.add_cog(Info(bot))
